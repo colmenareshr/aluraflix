0608ee0ac839b0e747c5ffe04188c11c479a9ec9
Humberto Colmenares <colmenareshr@gmail.com>
2023-04-14 15:02:37 -0300

    add global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Botao from './components/Botao';
+import { GlobalStyle } from './components/Global';
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyle />
       <h1>AluraFlix</h1>
       <Botao cta='Novo video' />
     </div>
---
+++ src/components/Global.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  
+  html,
+  body {
+    min-height: 100%;
+  }
+  
+  body {
+    font-family: Helvetica, sans-serif;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+  
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+  
+  input, button, textarea, select {
+    font: inherit;
+  }
+  
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+  
+  #root, #__next {
+    isolation: isolate;
+  }
+`;
