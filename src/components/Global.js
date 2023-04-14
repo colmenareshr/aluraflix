423342cfc35fb3a09e9b0f0bc41f27b85cc72670
Humberto Colmenares <colmenareshr@gmail.com>
2023-04-14 15:12:20 -0300

    add font family

--- src/components/Global.js
+++ src/components/Global.js
@@ -6,37 +6,41 @@ export const GlobalStyle = createGlobalStyle`
   *::before {
     box-sizing: border-box;
   }
-  
+
   * {
     margin: 0;
     padding: 0;
   }
-  
+
   html,
   body {
     min-height: 100%;
   }
-  
+
   body {
-    font-family: Helvetica, sans-serif;
+    font-family: Inter, sans-serif;
     line-height: 1.5;
     -webkit-font-smoothing: antialiased;
   }
-  
+
   img, picture, video, canvas, svg {
     display: block;
     max-width: 100%;
   }
-  
+
   input, button, textarea, select {
     font: inherit;
   }
-  
+
   p, h1, h2, h3, h4, h5, h6 {
     overflow-wrap: break-word;
   }
-  
+
   #root, #__next {
     isolation: isolate;
   }
 `;
+
+export const Colors = {
+  primary: ''
+}
