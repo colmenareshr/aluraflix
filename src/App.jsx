1cf5d830d822d3fcf5d5d25872fe8d036d456259
Humberto Colmenares <colmenareshr@gmail.com>
2023-04-17 19:09:58 -0300

    add hero section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from './components/Global';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Hero />
     </>
   );
 }
